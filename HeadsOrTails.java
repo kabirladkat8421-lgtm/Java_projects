36aa7bc5a2342fd4eddf76995fc231c102eabd57
Kabir <kabirladkat8421@gmail.com>
2026-07-24 14:07:06 +0530

    HeadsOrTails.java update Thread.sleep method used before it's was under 'if' statement know it's before 'if' statement

--- HeadsOrTails.java
+++ HeadsOrTails.java
@@ -7,7 +7,8 @@ import java.util.Random;
 public class HeadsOrTails extends Thread {
 
 	private static Object thread;
-        //May improve in future
+
+	// May improve in future
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		Random random = new Random();
@@ -15,9 +16,11 @@ public class HeadsOrTails extends Thread {
 		System.out.println("Flip the coin and Guess it's HEADS or TAILS");
 		System.out.println("HEADS OR TAILS..??");
 		String Flip = sc.nextLine();
+
+		Thread.sleep(5000);
+
 		boolean coin = random.nextBoolean();
 		if (coin) {
-			Thread.sleep(5000);
 			System.out.println("It's HEADS");
 		} else {
 			System.out.println("It's TAILS");
