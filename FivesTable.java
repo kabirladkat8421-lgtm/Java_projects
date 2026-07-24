dd710c87cb1d4b94988ed21c9d3a73373d122617
Kabir <kabirladkat8421@gmail.com>
2026-07-24 13:48:54 +0530

    Normal program using 'for' loop  FivesTable.java

---
+++ FivesTable.java
@@ -0,0 +1,14 @@
+package working.Java_projects.bin;
+
+public class FivesTable {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+
+			int num = 5 * i;
+			System.out.println("5*" + i + "=" + num);
+		}
+
+	}
+
+}
