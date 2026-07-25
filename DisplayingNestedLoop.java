0d0ca5319db98f90afc79e963af0c1cab519fd6d
Kabir <kabirladkat8421@gmail.com>
2026-07-25 13:13:12 +0530

    DisplayingNestedLoop.java update add 3rd nested for loop

--- DisplayingNestedLoop.java
+++ DisplayingNestedLoop.java
@@ -3,42 +3,64 @@ package working.Java_projects;
 public class DisplayingNestedLoop {
 
 	public static void main(String[] args) {
-		for (int i = 1; i <= 10; i++) {
+		int rows=10;
+		for (int i = 1; i <= rows; i++) 
+		{
+			{
+		}
 			for (int j = 1; j <= i; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
 
-		for (int k = 1; k <= 10; k++) {
-			for (int m = 1; m <= 5; m++) {
+		for (int a = 1; a <= rows; a++) {
+			for (int b = 1; b <= 5; b++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
+		{
+		for (int k = rows; k >= 1; k--) {
+			for (int m = 1; m <= k; m++) {
+				System.out.print("*");
+			}
+			System.out.println();
 	}
 }
-
+}
+}
 /*
- * Output:
- *
- **
- ***
- ****
- *****
- ******
- *******
- ********
- *********
- **********
- *****
- *****
- *****
- *****
- *****
- *****
- *****
- *****
- *****
- *****
+  Output:
+*
+**
+***
+****
+*****
+******
+*******
+********
+*********
+**********
+*****
+*****
+*****
+*****
+*****
+*****
+*****
+*****
+*****
+*****
+**********
+*********
+********
+*******
+******
+*****
+****
+***
+**
+*
+
  */
