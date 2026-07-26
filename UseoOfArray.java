a083fd94aae72cdac4b013741b39e5c4c05733ef
Kabir <kabirladkat8421@gmail.com>
2026-07-26 15:34:03 +0530

    UseOfArray.java done today

---
+++ UseoOfArray.java
@@ -0,0 +1,21 @@
+
+public class UseoOfArray {
+
+	public static void main(String[] args) {
+		int arr[] = { 10, 20, 30, 40, 50, 60, 70 };
+		int index = 0;
+
+		for (int a : arr)
+
+		{//Prefering modern way
+			System.out.println(a + " index is :" + index);
+			index++;
+		}
+	}
+
+}
+
+/*
+ * for(int i =0; i<arr.length;i++) {
+ * System.out.println(arr[i]+" index of array is: "+i); } } }
+ */
